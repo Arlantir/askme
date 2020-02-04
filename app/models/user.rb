660e1e1d6c893febbaa58a2390b78a8ea0693c4f
Arlantir <ror@worldoflosttime.ru>
2020-02-05 00:31:22 +0300

    добавлена валидация на уровне БД для юзернейма + валидация в модели срабатывает только на create and update

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ApplicationRecord
   validates_confirmation_of :password
 
   before_save :encrypt_password
-  before_validation :lower_case, on: :create
+  before_validation :lower_case, on: [:create, :update]
 
   private
 
