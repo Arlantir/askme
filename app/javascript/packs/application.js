2d467f16c24ca3031bb0c46e239e996badd0049b
Arlantir <ror@worldoflosttime.ru>
2020-02-08 01:22:39 +0300

    Add views

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,8 +6,14 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
-
+$(function () {
+  $('#ask-button').click(function () {
+    $('#ask-form').slideToggle(300);
+    return false;
+  });
+});
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
