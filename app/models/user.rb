25c99b95c8b11eb532f104d82086a98e5e02bce2
Arlantir <ror@worldoflosttime.ru>
2020-02-20 22:37:30 +0300

    fix validates color

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ApplicationRecord
   validates :password, on: :create, presence: true
   validates :password, confirmation: true
 
-  validates :color, format: { with: /\A#([A-F0-9]{6}){1,2}\z/i }, on: :update
+  validates :color, format: { with: /\A#([A-F0-9]{3}){1,2}\z/i }, on: :update
 
   before_save :encrypt_password
   before_validation :lower_case, on: [:create, :update]
