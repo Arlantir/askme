78ec4aaa99da8febd7009e164fd3af8f82bd68ee
Arlantir <ror@worldoflosttime.ru>
2020-03-22 23:10:58 +0300

    add model hash_tag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_16_002850) do
+ActiveRecord::Schema.define(version: 2020_03_22_200828) do
+
+  create_table "hash_tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
