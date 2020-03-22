e21a67ffc4ffade432d2324ec79803a727920c66
Arlantir <ror@worldoflosttime.ru>
2020-03-22 23:37:01 +0300

    settings models and add create hash_tags

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,22 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
+  has_many :question_hash_tags
+  has_many :hash_tags, through: :question_hash_tags
+
   validates :text, length: { maximum: 255 }, presence: true
+
+  after_commit :create_hash_tags, on: :create
+
+  private
+
+  def create_hash_tags
+    extract_name_hash_tags.each do |name|
+      hash_tags.create(name: name)
+    end
+  end
+
+  def extract_name_hash_tags
+    text.to_s.scan(/#\w+/).map{ |name| name.gsub("#", "") }
+  end
 end
